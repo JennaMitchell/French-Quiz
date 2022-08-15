d73922a06d9b9e324a9c1e7d08999d4a44d81262
Jenna Mitchell <jenaalymitchell@gmail.com>
2022-08-15 16:55:51 -0400

    updated scenario

--- src/pages/scenarios/scenarios-main.tsx
+++ src/pages/scenarios/scenarios-main.tsx
@@ -4,8 +4,20 @@ import {
   StyledConstructionIcon,
   ConstructionTextContainer,
 } from "./scenarios-main-styled-components";
-
+import { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "../../store/hooks";
+import { mainStoreSliceActions } from "../../store/store";
 const ScenariosMainPage = () => {
+  const dispatch = useAppDispatch();
+  const activePage = useAppSelector((state) => state.mainStore.activePage);
+
+  // useEffect
+  useEffect(() => {
+    if (activePage !== "Scenarios") {
+      dispatch(mainStoreSliceActions.setActivePage("Scenarios"));
+    }
+  });
+
   return (
     <TopContainer>
       <ConstructionInfoContainer>
